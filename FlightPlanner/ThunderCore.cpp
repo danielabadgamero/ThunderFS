35435fcbc79fe90fd5d2401ad698c5d63a6ec5f9
Daniel Abad Gamero <danielabadgamero@gmail.com>
2023-04-09 18:53:23 +0200

    Update cache.

--- FlightPlanner/ThunderCore.cpp
+++ FlightPlanner/ThunderCore.cpp
@@ -1,9 +1,6 @@
 #include <SDL.h>
 #include <SDL_image.h>
 #include <SDL_net.h>
-#include <imgui.h>
-#include <backends/imgui_impl_sdl2.h>
-#include <backends/imgui_impl_sdlrenderer.h>
 
 #include "ThunderCore.h"
 #include "ThunderMap.h"
@@ -18,12 +15,6 @@ void Thunder::init(const char* title)
 	SDL_GetCurrentDisplayMode(0, &monitor);
 	window = SDL_CreateWindow(title, SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, monitor.w, monitor.h, SDL_WINDOW_BORDERLESS | SDL_WINDOW_SHOWN);
 	renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_ACCELERATED | SDL_RENDERER_PRESENTVSYNC);
-	
-	IMGUI_CHECKVERSION();
-	ImGui::CreateContext();
-	ImGui::GetIO();
-	ImGui_ImplSDL2_InitForSDLRenderer(window, renderer);
-	ImGui_ImplSDLRenderer_Init(renderer);
 
 	running = true;
 
@@ -35,8 +26,6 @@ void Thunder::event()
 {
 	SDL_Event e;
 	while (SDL_PollEvent(&e))
-	{
-		ImGui_ImplSDL2_ProcessEvent(&e);
 		switch (e.type)
 		{
 		case SDL_QUIT:
@@ -51,7 +40,7 @@ void Thunder::event()
 			}
 			break;
 		case SDL_MOUSEMOTION:
-			if (ImGui::IsMouseDown(ImGuiMouseButton_Left))
+			if (SDL_GetMouseState(NULL, NULL) & SDL_BUTTON_LEFT)
 				Map::camera.move(e.motion.xrel, e.motion.yrel);
 			break;
 		case SDL_MOUSEWHEEL:
@@ -61,24 +50,17 @@ void Thunder::event()
 				Map::camera.zoomOut();
 			break;
 		}
-	}
 }
 
 void Thunder::draw()
 {
 	Map::updateTiles();
 
-	ImGui_ImplSDLRenderer_NewFrame();
-	ImGui_ImplSDL2_NewFrame();
-	ImGui::NewFrame();
-
-	ImGui::Render();
 	SDL_SetRenderDrawColor(renderer, 0x10, 0x10, 0x10, 0xFF);
 	SDL_RenderClear(renderer);
 
 	Map::draw();
 
-	ImGui_ImplSDLRenderer_RenderDrawData(ImGui::GetDrawData());
 	SDL_RenderPresent(renderer);
 }
 
@@ -86,10 +68,6 @@ void Thunder::quit()
 {
 	Net::close();
 	Map::saveCache();
-
-	ImGui_ImplSDLRenderer_Shutdown();
-	ImGui_ImplSDL2_Shutdown();
-	ImGui::DestroyContext();
 
 	SDL_DestroyWindow(window);
 	SDL_DestroyRenderer(renderer);
--- FlightPlanner/ThunderMap.cpp
+++ FlightPlanner/ThunderMap.cpp
@@ -1,6 +1,5 @@
 #include <cmath>
 #include <vector>
-#include <algorithm>
 #include <unordered_map>
 #include <fstream>
 
